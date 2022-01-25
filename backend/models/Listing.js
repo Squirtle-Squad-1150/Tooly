d08edef982df397e32e83b6525797bdfd8ebdb34
rayhaynes504 <twotone504@gmail.com>
2022-01-25 09:11:59 -0600

    Add Listing model

---
+++ backend/models/Listing.js
@@ -0,0 +1,18 @@
+const mongoose = require('../db/connection');
+
+const ListingSchema = new mongoose.Schema({
+	title: String,
+	description: String,
+	brand: String,
+	model: String,
+	condition: String,
+	cordless: Boolean,
+	category: String,
+	price: Number,
+	location: String,
+	available: Boolean,
+});
+
+const Listing = mongoose.model('Listing', ListingSchema);
+
+module.exports = Listing;
