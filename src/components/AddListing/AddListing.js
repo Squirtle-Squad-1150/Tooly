b6fc25374d8da2bc73e9e4581ec599bbfa6eff91
rayhaynes504 <twotone504@gmail.com>
2022-01-31 11:23:36 -0600

    Add values to dropdown options

--- src/components/AddListing/AddListing.js
+++ src/components/AddListing/AddListing.js
@@ -5,11 +5,11 @@ import { useNavigate } from 'react-router-dom';
 
 function UpdateTool(props) {
 	const defaultData = {
-		tool: '',
+		tool: 'Air Compressor',
 		brand: '',
 		model: '',
-		category: '',
-		condition: '',
+		category: 'Compressors',
+		condition: 'Excellent',
 		cordless: Boolean,
 		available: Boolean,
 		location: '',
@@ -48,6 +48,7 @@ function UpdateTool(props) {
 							id='tool'
 							name='tool'
 							value={formData.tool}
+							// defaultValue={'Air Compressor'}
 							onChange={(event) =>
 								setFormData({ ...formData, tool: event.target.value })
 							}>
