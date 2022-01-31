9166be00ab51a363f8e70d157203fa6ab35b4a6e
rayhaynes504 <twotone504@gmail.com>
2022-01-31 09:49:14 -0600

    Add fetch to tool container component

--- src/components/ToolContainer/ToolContainer.js
+++ src/components/ToolContainer/ToolContainer.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import ToolCard from '../ToolCard/ToolCard';
 import './toolContainer.css';
-import { useContext } from 'react';
+import { useContext, useEffect } from 'react';
 import { toolContext } from '../../toolContext';
+import axios from 'axios';
 
 function ToolContainer(props) {
-	const { tools } = useContext(toolContext);
+	const { tools, setTools } = useContext(toolContext);
+	// const navigate = useNavigate()
+	const url = 'https://cryptic-dusk-16798.herokuapp.com/listing';
+
+	useEffect(() => {
+		axios.get(url).then((res) => {
+			setTools(res.data);
+			// navigate('/');
+		});
+	}, []);
 
 	return <div className='toolContainer'>{<ToolCard tools={tools} />}</div>;
 }
